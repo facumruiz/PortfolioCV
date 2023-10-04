d6ec9aea1f41f7722b160f94e73d32901055f6c4
facumruiz <facundoruiz105@gmail.com>
2023-10-04 17:12:04 -0300

    add captcha

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const form = document.getElementById("contact-form");
 const formEvent = form.addEventListener("submit", (event) => {
   event.preventDefault();
   let mail = new FormData(form);
-  sendMail(mail);
-  alert("email sent successfully")
-  form.reset()
+  const captchaResponse = grecaptcha.getResponse()
+  if (!captchaResponse.length > 0) {
+    //throw new Error("Captcha not complete")
+    document.getElementById("errorCaptcha").innerHTML="🛑Captcha not complete🛑";
+  } else {
+
+    sendMail(mail);
+    //alert("email sent successfully")
+    document.getElementById("errorCaptcha").innerHTML="✅Email sent successfully. thks😉";
+    form.reset()
+
+  }
+
 });
 
 const sendMail = (mail) => {
@@ -22,13 +32,13 @@ const sendMail = (mail) => {
 var check = document.querySelector(".check");
 check.addEventListener('click', idioma);
 
-function idioma(){
-    let id = check.checked;
+function idioma() {
+  let id = check.checked;
 
-    if(id==true){
-        location.href="index(en).html"
-    }else{
-        location.href="index.html"
-    }
+  if (id == true) {
+    location.href = "index(en).html"
+  } else {
+    location.href = "index.html"
+  }
 }
 
